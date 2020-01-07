dabe393c5379d772b665a368f5ead0da4320ba8b
lxiaog <15618398056@163.com>
2020-01-07 18:16:10 +0800

    spring+springmvc+mybatis+logback+generator demo

---
+++ demo-springmvc-mybatis/src/main/java/org/example/demo/repository/dao/PersonMapper.java
@@ -0,0 +1,17 @@
+package org.example.demo.repository.dao;
+
+import org.example.demo.repository.entity.Person;
+
+public interface PersonMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Person record);
+
+    int insertSelective(Person record);
+
+    Person selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Person record);
+
+    int updateByPrimaryKey(Person record);
+}
